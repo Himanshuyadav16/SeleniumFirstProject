4c2a5cf05ba82a308aebe43fe74314a87431209e
Himanshu Yadav <himanshuyadav1607@gmail.com>
2023-06-28 23:23:25 +0530

    first commit

---
+++ src/test/java/com/selenium/SeleniumTest.java
@@ -0,0 +1,21 @@
+package com.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("https://www.facebook.com/");
+
+        driver.findElement(By.id("email")).sendKeys("yourusername@gmail.com");
+        driver.findElement(By.id("pass")).sendKeys("password");
+        Thread.sleep(3000);
+        driver.findElement(By.name("login")).click();
+
+    }
+}
