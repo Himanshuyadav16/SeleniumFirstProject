0639678e3c26a76e17c9cda653575cde6f90025d
Himanshu Yadav <himanshuyadav1607@gmail.com>
2023-07-01 11:41:06 +0530

    gitignorefile

--- src/test/java/com/selenium/SeleniumFacebookRegister.java
+++ src/test/java/com/selenium/SeleniumFacebookRegister.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-
 public class SeleniumFacebookRegister {
     public static void main(String[] args) throws InterruptedException {
 
