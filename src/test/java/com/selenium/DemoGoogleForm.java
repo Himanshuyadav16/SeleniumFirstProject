2c2b0ede79a9242c31fe1c0920e256320724ff2b
Himanshu Yadav <himanshuyadav1607@gmail.com>
2023-07-03 23:34:34 +0530

    first commit

--- src/test/java/com/selenium/DemoGoogleForm.java
+++ src/test/java/com/selenium/DemoGoogleForm.java
@@ -4,20 +4,82 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
-public interface DemoGoogleForm {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chorme.driver", "src/main/resources/chromedriver");
 
-        WebDriver driver = new ChromeDriver();
+import java.time.Duration;
 
-        driver.get("https://www.techlistic.com/p/selenium-practice-form.html");
+public class DemoGoogleForm {
+    public String baseUrl = ApplicationProperties.INSTANCE.getUrl();
+    String driverPath = "src/main/resources/chromedriver";
+    public WebDriver driver ;
+    @BeforeSuite
+    public void beforeSuite() {
+        System.setProperty("webdriver.chorme.driver", driverPath);
+        driver = new ChromeDriver();
+        driver.get(baseUrl);
+        System.out.println(baseUrl);
+    }
+    @Test
+    public void GetTest() {
 
-        WebElement firstName= driver.findElement(By.name("firstname"));
+        WebElement firstName = driver.findElement(By.cssSelector("input[name='firstname']"));
         firstName.sendKeys("Rohit");
 
-        WebElement lastName=driver.findElement(By.name("lastname"));
+        WebElement lastName = driver.findElement(By.name("lastname"));
         lastName.sendKeys("Sharma");
 
+        WebElement gender = driver.findElement(By.id("sex-0"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.elementToBeClickable(gender));
+        gender.click();
+
+        WebElement yearOfExperience = driver.findElement(By.id("exp-2"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("exp-2")));
+        yearOfExperience.click();
+
+
+        WebElement datePicker = driver.findElement(By.id("datepicker"));
+        datePicker.sendKeys("03/07/2023");
+
+        WebElement profession = driver.findElement(By.name("profession"));
+        WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        wait1.until(ExpectedConditions.elementToBeClickable(profession));
+        profession.isSelected();
+        profession.click();
+
+        WebElement tool = driver.findElement(By.id("tool-0"));
+        wait1.until(ExpectedConditions.elementToBeClickable(tool));
+        tool.isSelected();
+        tool.click();
+
+        WebElement continents = driver.findElement(By.name("continents"));
+        continents.click();
+        Select selectContinents = new Select(continents);
+        selectContinents.selectByVisibleText("Africa");
+        wait1.until(ExpectedConditions.elementToBeClickable(continents));
+        continents.click();
+
+        WebElement command = driver.findElement(By.id("selenium_commands"));
+        Select selectCommand = new Select(command);
+        selectCommand.selectByVisibleText("WebElement Commands");
+        wait1.until(ExpectedConditions.elementToBeClickable(command));
+        command.click();
+
+//        WebElement image= driver.findElement(By.id("photo"));
+//        image.click();
+
+        WebElement sumbit = driver.findElement(By.id("submit"));
+        sumbit.click();
+    }
+        @AfterSuite
+        public void afterSuite() {
+        //    driver.close();
     }
 }
